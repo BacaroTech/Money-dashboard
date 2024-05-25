2877659cab07c3b993e6fc4c66cfb16828bfa7ce
Giorgio Basile <56266079+Ghita00@users.noreply.github.com>
2024-05-25 08:06:10 +0200

    initial commit FE

---
+++ FE/src/assets/palette.ts
@@ -0,0 +1,12 @@
+const LIGHTBUE = "#133C55";
+const DARKBLUE = "#386FA4";
+const YELLOW = "#ECC30B";
+const ORANGE = "#F37748";
+const RED = "#D56062";
+const GREEN = "#35FF69";
+const BLACK = "#000000";
+const WHITE = "#FFFFFF";
+
+export {
+    LIGHTBUE, DARKBLUE, YELLOW, ORANGE, RED, GREEN, BLACK, WHITE
+}
