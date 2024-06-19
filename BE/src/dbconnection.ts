bb52095328b0cee06132dd5e60efc5b22e38c0c4
Giorgio Basile <56266079+Ghita00@users.noreply.github.com>
2024-06-19 19:42:07 +0200

    inserted node server into docker compose

--- BE/src/dbconnection.ts
+++ BE/src/dbconnection.ts
@@ -2,7 +2,7 @@ const { Client } = require('pg');
 const client = new Client({
 	user: 'postgres',
 	password: 'postgres',
-	host: 'localhost',
+	host: 'moneydashboard_postgres_db',
 	port: '5432',
 	database: 'postgres',
 });
