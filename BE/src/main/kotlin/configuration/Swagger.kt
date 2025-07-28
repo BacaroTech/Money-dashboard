c9be3330764e0c8f3a5317b33fc96317776745bf
FrigoTurcoMoreno <frigoturcomoreno@gmail.com>
2025-07-28 23:27:56 +0200

    fix swagger

--- BE/src/main/kotlin/configuration/Swagger.kt
+++ BE/src/main/kotlin/configuration/Swagger.kt
@@ -6,8 +6,8 @@ import io.ktor.server.plugins.swagger.*
 import io.ktor.server.routing.*
 
 fun Application.configureSwagger() {
-//    routing {
-//        openAPI(path = "openapi", swaggerFile = "openapi.yaml")
-//        swaggerUI(path = "swagger-ui", swaggerFile = "openapi.yaml")
-//    }
+    routing {
+        openAPI(path = "openapi", swaggerFile = "openapi.yaml")
+        swaggerUI(path = "swagger-ui", swaggerFile = "openapi.yaml")
+    }
 }
